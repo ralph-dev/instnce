24e94d8adb32e3c9893de78d5f2b96c3fe08ff64
nafeesrawji <nafees.rawji@hotmail.com>
2018-03-08 15:47:15 -0500

    Configured eslint

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const cors = require('cors');
-const morgan = require("morgan");
-const path = require("path");
+const morgan = require('morgan');
+const path = require('path');
 
 const axios = require('axios');
 
 const port = process.env.port || 8080;
 
 const weatherApi = axios.create({
-    baseURL: "https://api.darksky.net/forecast/",
+    baseURL: 'https://api.darksky.net/forecast/',
     params: {
-        lang: "en",
-        units: "ca",
-        exclude: ["hourly", "minutely"]
+        lang: 'en',
+        units: 'ca',
+        exclude: ['hourly', 'minutely']
     }
 });
 
@@ -21,28 +22,30 @@ const weatherApi = axios.create({
 // app.use(express.static(path.join(__dirname, "/build")));
 app.use(morgan('tiny'));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 app.get('/getWeather', async (req, res) => {
-    let lat = req.query.lat;
-    let long = req.query.long;
-    let url = `/352a2b441280de2d20e504c4692119fb/${lat},${long}`;
+    const { lat } = req.query;
+    const { long } = req.query;
+    const url = `/352a2b441280de2d20e504c4692119fb/${lat},${long}`;
     if (lat === undefined || long === undefined) {
-        res.status(400).send("Requires latitude and longitude");
-    } else {
+        res.status(400).send('Requires latitude and longitude');
+    }
+    else {
         try {
-            let doc = await weatherApi(url);
+            const doc = await weatherApi(url);
             res.send(doc.data);
-        } catch (err) {
+        }
+        catch (err) {
             console.log(err);
-            res.status(500).send("Could not get data");
+            res.status(500).send('Could not get data');
         }
     }
 });
 
 
-app.get("/", (req, res) => {
-   res.send("Welcome to instnce server");
+app.get('/', (req, res) => {
+    res.send('Welcome to instnce server');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
