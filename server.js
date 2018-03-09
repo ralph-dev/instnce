541e4ec234f6ce377c6fbf6b4e08e34a0a37d5e1
nafeesrawji <nafees.rawji@hotmail.com>
2018-03-08 22:51:54 -0500

    Started github integration

--- server.js
+++ server.js
@@ -10,27 +10,20 @@ const app = express();
 // middleware
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'bobross', resave: true, saveUninitialized: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(morgan('tiny'));
-app.use(cors({
-    origin: function (origin, callback) {
-        callback(null, true);
-    }, credentials : true,
-    optionsSuccessStatus: 200
-}));
+app.use(cors());
 // app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 // routes
-const auth = require('./routes/auth');
 const weather = require('./routes/weather');
 const github = require('./routes/github');
 
 // routes
 app.use('/weather', weather);
 app.use('/github', github);
-app.use('/auth', auth);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Instnce server');
