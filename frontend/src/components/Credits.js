73c9a1bcd05a844fe130748a2ebfcc59122ca589
Simon <simon.ngo@mail.utoronto.ca>
2018-03-17 00:28:34 -0400

    added credits page

---
+++ frontend/src/components/Credits.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Credits extends Component {
+  render(){
+    return(
+      <div className="widget credits">
+        <h1>Credits</h1>
+        <div className="links">Replace me with sources</div>
+      </div>
+    );
+  }
+}
+export default Credits;
