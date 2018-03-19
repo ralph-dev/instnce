01720879b8b561031d6bd92b0c5257d82a06774d
nafeesrawji <nafees.rawji@hotmail.com>
2018-03-18 23:28:20 -0400

    Connected spotify reducer

--- frontend/src/redux/reducers/indexReducer.js
+++ frontend/src/redux/reducers/indexReducer.js
@@ -2,11 +2,13 @@ import {combineReducers} from "redux";
 import weatherReducer from "./weatherReducer";
 import githubReducer from "./githubReducer";
 import settingsReducer from "./settingsReducer";
+import spotifyReducer from "./spotifyReducer";
 
 const indexReducer = combineReducers({
     weather:weatherReducer,
     github: githubReducer,
-    settings: settingsReducer
+    settings: settingsReducer,
+    spotify: spotifyReducer
 });
 
 export default indexReducer;
