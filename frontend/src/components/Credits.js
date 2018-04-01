6797a4ac4f707d89a292076ac3406aa1f974fbcd
Simon <simon.ngo@mail.utoronto.ca>
2018-04-01 16:04:51 -0400

    enabled credits page

--- frontend/src/components/Credits.js
+++ frontend/src/components/Credits.js
@@ -5,7 +5,11 @@ class Credits extends Component {
     return(
       <div className="widget credits">
         <h1>Credits</h1>
-        <div className="links">Replace me with sources</div>
+        <div className="links">
+          <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+          <div>I18next snippets from <a href="https://github.com/i18next/react-i18next/tree/master/example/react_renderProps/src">https://github.com/i18next/react-i18next/tree/master/example/react_renderProps/src</a></div>
+
+        </div>
       </div>
     );
   }
--- frontend/src/components/quick-links/QuickLinks.js
+++ frontend/src/components/quick-links/QuickLinks.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import QuickLink from "./QuickLink";
 
-import { home, githubAlt, briefcase, spotify, stickyNoteO, gear} from 'react-icons-kit/fa/';
-import {GITHUB, HOME, NOTES, SETTINGS, SPOTIFY, JIRA} from "../../routes/index/Dashboard";
+import { home, githubAlt, briefcase, spotify, stickyNoteO, gear, book} from 'react-icons-kit/fa/';
+import {GITHUB, HOME, NOTES, SETTINGS, SPOTIFY, JIRA, CREDITS} from "../../routes/index/Dashboard";
 
 
 class QuickLinks extends React.Component {
@@ -14,7 +14,8 @@ class QuickLinks extends React.Component {
           {icon: briefcase, step: JIRA},
           {icon: spotify, step: SPOTIFY},
           {icon: stickyNoteO, step: NOTES},
-          {icon: gear, step: SETTINGS}
+          {icon: gear, step: SETTINGS},
+          {icon: book, step: CREDITS}
         ];
     }
 
