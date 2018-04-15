f45c86b837faede76e738707a8d4c0d71d698fc8
nafees <nafees.rawji@mail.utoronto.ca>
2018-04-14 23:19:16 -0400

    Updated heartbeat

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.port || 8080,
-    GITHUB_KEY: '',
+    GITHUB_KEY: '0c82753fb997b667da51',
     GITHUB_SECRET: 'eda3630a38cd9b9c19b0cb97c436d6f122a2bc8e',
     SPOTIFY_KEY: '086698f2264e46d0b8707062491f198f',
     SPOTIFY_SECRET: '90c1672438a44e0f8aaafe0d7eb39ba8'
